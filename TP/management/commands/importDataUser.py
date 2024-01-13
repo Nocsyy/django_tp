4c30db38da8f8385d7d95808c22c3508a7e159f8
Andy <andy@MacAir.local>
2024-01-13 18:02:51 +0100

    Commit

--- TP/management/commands/importDataUser.py
+++ TP/management/commands/importDataUser.py
@@ -17,20 +17,27 @@ class Command(BaseCommand):
 
         # Iterer sur les lignes du dataframe et ajouter les instances à la liste
         for index, row in tqdm(data.iterrows(), desc='Importation des données', total=len(data)):
-            # Créer un utilisateur Django
-            mon_modele_instance = User(
-                password=row['password'],
-                last_login=pd.to_datetime(row['last_login'], errors='coerce'),
-                is_superuser=row['is_superuser'],
-                username=row['username'],
-                first_name=row['first_name'],
-                last_name=row['last_name'],
-                email=row['email'],
-                is_staff=row['is_staff'],
-                is_active=row['is_active'],
-                date_joined=pd.to_datetime(row['date_joined'], errors='coerce'),
-            )
-            instances_to_create.append(mon_modele_instance)
+    # Vérifier si l'utilisateur existe déjà
+            existing_user = User.objects.filter(username=row['username']).first()
+
+            if existing_user is None:
+                # L'utilisateur n'existe pas, donc on peut le créer
+                mon_modele_instance = User(
+                    password=row['password'],
+                    last_login=pd.to_datetime(row['last_login'], errors='coerce'),
+                    is_superuser=row['is_superuser'],
+                    username=row['username'],
+                    first_name=row['first_name'],
+                    last_name=row['last_name'],
+                    email=row['email'],
+                    is_staff=row['is_staff'],
+                    is_active=row['is_active'],
+                    date_joined=pd.to_datetime(row['date_joined'], errors='coerce'),
+                )
+                instances_to_create.append(mon_modele_instance)
+            else:
+                # L'utilisateur existe déjà, vous pouvez choisir de mettre à jour les données existantes si nécessaire
+                pass
 
         User.objects.bulk_create(instances_to_create)
 
